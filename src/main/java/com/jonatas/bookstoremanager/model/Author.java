809d996d32d3fc4c11af217759cfabf1d26ed832
Jônatas Oliveira Vasconcelos <j.olivasc@outlook.com>
2021-04-06 21:43:50 -0300

    Models author and book created with lombok and jpa  annotation

---
+++ src/main/java/com/jonatas/bookstoremanager/model/Author.java
@@ -0,0 +1,33 @@
+package com.jonatas.bookstoremanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books = new ArrayList<>();
+
+
+}
---
+++ src/main/java/com/jonatas/bookstoremanager/model/Book.java
@@ -0,0 +1,40 @@
+package com.jonatas.bookstoremanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer page;
+
+    @Column(nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(nullable = false, name = "publisher_name", unique = true)
+    private String publicherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+}
